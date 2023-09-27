64e46594fda1d4a8ae03b75d7bce20ff74dd15bf
jiechen <1487503910@qq.com>
2023-09-27 14:21:15 +0800

    chore: remove useless code

--- docs/.vitepress/route.ts
+++ docs/.vitepress/route.ts
@@ -1,30 +1,34 @@
 import results from "../../scripts/autoGenerateRoutes";
-console.log("result---", JSON.stringify(results));
 
 export const navItems = [
 	{ text: "Home", link: "/" },
 	{ text: "Examples", link: "/examples/markdown-examples" },
 ];
 
+/**
+ * @description auto-generate routes
+ * @example
+		[
+			{
+				text: "Examples",
+				items: [
+					{ text: "Markdown Examples", link: "/markdown-examples" },
+					{ text: "Runtime API Examples", link: "/api-examples" },
+				],
+			},
+			{
+				text: "Vue",
+				items: transformSidebarItems([
+					["Geeker Admin", "/vue/geeker-admin"],
+					["Element Plus", "/vue/element-plus"],
+				]),
+			},
+			{
+				text: "React",
+				items: transformSidebarItems([["Maskbook", "/react/maskbook"]]),
+			},
+		];
+ */
 export const sidebarItems = [...results]
 
-// export const sidebarItems = [
-// 	{
-// 		text: "Examples",
-// 		items: [
-// 			{ text: "Markdown Examples", link: "/markdown-examples" },
-// 			{ text: "Runtime API Examples", link: "/api-examples" },
-// 		],
-// 	},
-// 	{
-// 		text: "Vue",
-// 		items: transformSidebarItems([
-// 			["Geeker Admin", "/vue/geeker-admin"],
-// 			["Element Plus", "/vue/element-plus"],
-// 		]),
-// 	},
-// 	{
-// 		text: "React",
-// 		items: transformSidebarItems([["Maskbook", "/react/maskbook"]]),
-// 	},
-// ];
+
